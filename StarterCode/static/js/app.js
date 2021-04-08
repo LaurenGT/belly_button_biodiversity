26aa7e7e92d238d36252784cb7ce6ea1025943dd
LaurenGT <ltoothaker2@gmail.com>
2021-04-08 14:01:25 -0700

    demographic info populating. cleaned code and added comments

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,44 +1,33 @@
-// d3.json('samples.json').then(data => {
-//     console.log(data);
-// })
-
-// on load (function init()), the charts and information is showing for the first person in the list
-
+// build out init function to run on load with the first sample entry
 function init() {
     // access the dropdown menu so we can populate it from the data
     let dropdownMenu = d3.select("#selDataset");
     
     // access json data to pull the names to populate dropdown
     d3.json('samples.json').then(data => {
-        // console.log(data);
     
         // access each name from json data and append their value to the options in the dropdownMenu
         data.names.forEach(function(name) {
             dropdownMenu.append("option").text(name).property("value");
         });
-        // build out plotting functions to reference here and show the data on load
+        // reference lower functions here and show the data on load
         getPlotData(data.names[0]);
+        getMetaData(data.names[0])
     });
 };
 
 // build out function to gather plot data from json data
-
-
-// just calling first entry
 function getPlotData(sampleID) {
+    // access json file
     d3.json('samples.json').then(data => {
         
+        // access and collect the samples data
         let subjectData = data.samples.filter(idData => idData.id === sampleID)[0];
         let sampleValues = subjectData.sample_values;
         let otuIDs = subjectData.otu_ids;
         let otuLabels = subjectData.otu_labels;
 
-        console.log(subjectData)
-        // console.log(sampleValues);
-        // console.log(otuIDs);
-        // console.log(otuLabels);
-
-        // fill the plot
+        // build out bar chart
         let trace1 = [{
             type: 'bar',
             x: sampleValues.slice(0,10).reverse(),
@@ -46,9 +35,9 @@ function getPlotData(sampleID) {
             orientation: 'h',
             
         }];
-
         Plotly.newPlot('bar',trace1)
 
+        // build out bubble chart
         let trace2 = [{
             x: otuIDs,
             y: sampleValues,
@@ -59,15 +48,34 @@ function getPlotData(sampleID) {
                 color: otuIDs,
             }
         }]
-
         Plotly.newPlot('bubble', trace2)
-
     });
 };
 
+// build function to gather demographic data
+function getMetaData(sampleID) {
+    // access the json file
+    d3.json('samples.json').then(data => {
+    // access the metadata within the json file and filter on ID
+    let metaData = data.metadata
+    let filterResult = metaData.filter(sampleObj => sampleObj.id == sampleID);
+    let result = filterResult[0];
 
+    // select the HTML section the panel is held in
+    let panel = d3.select("#sample-metadata")
+    // clear any current data
+    panel.html("")
+    // populate the demographics panel
+    Object.entries(result).forEach(([key, value]) => {
+        panel.append("p").text(`${key.toUpperCase()}: ${value}`);
+      });
+    });
+};
+
+// build formula to match the referenced formula in HTML code to update charts when subject Id is changed
 function optionChanged(sampleID) {
     getPlotData(sampleID);
+    getMetaData(sampleID);
 };
 
 init();
